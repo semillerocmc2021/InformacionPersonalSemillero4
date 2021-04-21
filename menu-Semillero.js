c6fd1eb1936fb7e8c12e3bba1a1489aa8b30d90a
Danthology96 <daniel.andres.novillo@gmail.com>
2021-04-20 19:34:51 -0500

    versión inicial

---
+++ menu-Semillero.js
@@ -0,0 +1,32 @@
+{
+    "COMMONS": {
+        "MENU": {
+			"MNU_DANT_BVDA":"Bienvenida Daniel Novillo",
+			"MNU_SMCMC":"Formularios Semillero",
+			"MNU_DANT_CALF":"Calificaciones Daniel Novillo",
+			"MNU_DANT_CALC":"Calculadora Daniel Novillo",
+			"Evaluacion2_DANT":"Evaluacion 2 Daniel Novillo",
+			"MNU_EVALUACION":"Evaluacion 2 Forms",
+			"Empresa_DANT": "Empresa DANT",
+			"ClientesFrm_DANT": "Clientes Form DANT",
+			"BusClientes_DANT": "Buscar Clientes DANT",
+			"RecupClientes_DANT": "Recuperar Clientes DANT",
+			"Daniel_Novillo": "Daniel Novillo",
+			"INS_PROD": "Insertar Producto",
+			"CONS_PROD": "Consultar Productos",
+			"CONS_CLIENT": "Consultar Clientes",
+			"MNU_DANT2_BVDA": "Bienvenida 2 Daniel Novillo",
+			"CALF_DANT2": "Calificaciones 2 Daniel Novillo",
+			"CALC_DANT2": "Calculadora 2 Daniel Novillo",
+			"EVA2_DANT2": "Evaluacion 2 Daniel Novillo 2",
+			"CCliente_DANT2": "Crear Cliente DANT2",
+			"BCliente_DANT2": "Buscar Cliente DANT2",
+			"RCliente_DANT2": "Recuperar Clientes DANT2",
+			"INSP_DANT2": "Insertar Producto DANT",
+			"MNU_DANT2": "MENU DANT",
+			"CONSP_DANT2": "Consultar Productos DANT",
+			"CONSC_DANT2": "Buscar Cliente DANT2"
+				
+        }
+    }
+}
